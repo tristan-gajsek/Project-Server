a998c56d3e6558c3ae0c2708cc750292854b94c6
tristan-gajsek <tristan.gajsek@student.um.si>
2024-05-31 04:37:51 +0200

    PROJECT-91 Add the SSH port to the allow list

--- setup/firewall.sh
+++ setup/firewall.sh
@@ -3,6 +3,7 @@
 apt-get install ufw
 ufw enable
 ufw default deny incoming
-ufw allow 3000
-ufw allow 3001
-ufw allow 1883
+ufw allow 22   # SSH
+ufw allow 3000 # React/Express
+ufw allow 3001 # React/Express
+ufw allow 1883 # MQTT
