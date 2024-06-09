e1533c50a1c39e42d28bb6463cea49b9a91fc6df
tristan-gajsek <tristan.gajsek@student.um.si>
2024-06-09 13:24:35 +0200

    Nevermind, I'm blind

--- setup/firewall.sh
+++ setup/firewall.sh
@@ -4,9 +4,8 @@ apt-get install ufw
 ufw enable
 ufw default deny incoming
 ufw allow 22   # SSH
-ufw allow 3000 # React
-ufw allow 3001 # Express
-ufw allow 5000 # Flask
+ufw allow 3000 # React/Express
+ufw allow 3001 # React/Express
 ufw allow 1883 # MQTT (TCP)
 ufw allow 8888 # MQTT (WS)
 ufw allow 5000 # FaceID API
